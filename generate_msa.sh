2da1f8c80c9570a0f8cbbbb4e0509e5d244263ad
Charlotte Rochereau <charlotte.rochereau@gmail.com>
2021-09-20 15:54:53 -0400

    Fixed bug in arg parser.

--- generate_msa.sh
+++ generate_msa.sh
@@ -5,22 +5,21 @@
 
 
 # arguments passed to script
-while getopts f:i:o:h:c:i: arg
+while getopts f:o:r:c:i: arg
 do
     case "${arg}" in
     f) # Specify input file (Uniprot ids <> orthoDB ids)
 	    infile=${OPTARG};;
     o) # Specify output directory, where MSA are saved
 	    outdir=${OPTARG};;
-	r) # Specify directory to FASTA files of orthologous sequences
+    r) # Specify directory to FASTA files of orthologous sequences
 	    orthologs_dir=${OPTARG};;
     c) # Specify sequence coverage
 	    cov=${OPTARG};;
-	i) # Specify sequence identity %
+    i) # Specify sequence identity %
 	    id=${OPTARG};;
     esac
 done
-
 
 while IFS=$',' read -r -a myArray
 do
@@ -32,16 +31,16 @@ do
     uniprot_url="https://www.uniprot.org/uniprot/${uniprot_id}.fasta"
     wget "$uniprot_url"
 
-    orthologs_file="${orthologs_dir}/${uniprot_id}_orthologs.fasta"
+    orthologs="${orthologs_dir}/${uniprot_id}_orthologs.fasta"
 
     # align sequences
-    jackhmmer --noali --notextw --cpu 1 --incT 100 -T 100 -N 4 -A "${uniprot_id}.sto" "${uniprot_id}.fasta" "${orthologs_file}"
+    jackhmmer --noali --notextw --cpu 1 --incT 100 -T 100 -N 4 -A "${uniprot_id}.sto" "${uniprot_id}.fasta" "$orthologs"
 
     # convert sto file to fas file
     perl s2f.pl "${uniprot_id}.sto" "${uniprot_id}.fas"
 
     # filter sequences by identity and coverage
-    hhfilter -i "${uniprot_id}.fas" -o "${outdir}/${uniprot_id}.i${id}c${cov}.diff512.a3m" -i "$id" -c "$cov" -M first -diff 512 
+    hhfilter -i "${uniprot_id}.fas" -o "${outdir}/${uniprot_id}.i${id}c${cov}.diff512.a3m" -id "$id" -cov "$cov" -M first -diff 512
 
     # remove tmp files
     rm "${uniprot_id}.fasta" "${uniprot_id}.sto" "${uniprot_id}.fas"
